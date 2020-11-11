976102daa5ff2fa6018947ba89cbd51047779668
Danish Naseem <danishnaseem05@gmail.com>
2020-11-10 18:31:45 -0600

    vanpool created functionality added

--- app/controllers/vanpools_controller.rb
+++ app/controllers/vanpools_controller.rb
@@ -1,13 +1,21 @@
 class VanpoolsController < ApplicationController
   before_action :set_current_user
 
+  def vanpool_params
+    params.require(:vanpool).permit(:name, :cost, :max_capacity, :start_time, :end_time, :pickup_location, :dropoff_location)
+  end
+
   def index
+    @vanpools = Vanpool.all
   end
 
   def new
   end
 
   def create
+    v_name = vanpool_params[:name]
+    flash[:notice] = "Vanpool #{v_name} successfully created"
+    Vanpool.create!(vanpool_params)
     redirect_to vanpools_index_path
   end
 
