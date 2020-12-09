0fee3f732195e6ece1883b29d954b33594047ed7
Danish Naseem <danishnaseem05@users.noreply.github.com>
2020-12-09 15:38:02 -0600

    Volunteer to drive works

--- app/controllers/vanpools_controller.rb
+++ app/controllers/vanpools_controller.rb
@@ -36,6 +36,11 @@ class VanpoolsController < ApplicationController
 
   #TODO make rider and driver join seperately
   def join
+    # If redirect_to_welcome is false, then redirect_to vanpools_index_path;
+    # Otherwise, redirect_to pages_welcome_path, both with the output_msg inside flash[:notice]
+    output_msg = " "
+    redirect_to_welcome = false
+
     vanpool_ids = @current_user[:rider_vanpool_ids]
     if @current_user.nil?
       flash[:notice] = "user is nil"
@@ -45,28 +50,72 @@ class VanpoolsController < ApplicationController
       flash[:notice] = "47 vanpool_ids is nil", @current_user
       redirect_to pages_welcome_path and return
     end
-    params[:vanpools].keys.each do |vid|
-      vid = vid.to_i
-      added = false
-      if vanpool_ids.nil?
-        flash[:notice] = "57 vanpool_ids is nil"
-        redirect_to pages_welcome_path and return
-      end
-      vanpool_ids.each do |id|
-        if id == vid
-          added = true
+
+    # Rider
+    if !params[:vanpools].nil?
+      params[:vanpools].keys.each do |vid|
+        vid = vid.to_i
+        added = false
+        if vanpool_ids.nil?
+          flash[:notice] = "57 vanpool_ids is nil"
+          redirect_to pages_welcome_path and return
+        end
+        vanpool_ids.each do |id|
+          if id == vid
+            added = true
+          end
+        end
+        if !added
+          @current_user.rider_vanpool_ids_will_change!
+          @current_user.rider_vanpool_ids << vid
+          vanpool = Vanpool.find_by(id: vid)
+          vanpool.current_capacity = vanpool.current_capacity + 1
+          vanpool.save
+          @current_user.save
         end
       end
-      if !added
-        @current_user.rider_vanpool_ids_will_change!
-        @current_user.rider_vanpool_ids << vid
-        vanpool = Vanpool.find_by(id: vid)
-        vanpool.current_capacity = vanpool.current_capacity + 1
-        vanpool.save
-        @current_user.save
-      end
+      output_msg = output_msg + "#{@current_user.user_id}, you successfully joined the vanpool(s). "
+      redirect_to_welcome = true
+    elsif params[:vanpools].nil?
+      output_msg = output_msg + "No vanpool selected to ride "
     end
-    flash[:notice] = "#{@current_user.user_id}, you successfully joined the vanpool(s)."
-    redirect_to pages_welcome_path
+
+    # Driver
+    driver_vanpool_ids = @current_user[:driver_vanpool_ids]
+    if !params[:vanpools_driver].nil?
+      params[:vanpools_driver].keys.each do |vid|
+        vid = vid.to_i
+        added = false
+        if driver_vanpool_ids.nil?
+          flash[:notice] = "57 vanpool_ids is nil"
+          redirect_to pages_welcome_path and return
+        end
+        vanpool = Vanpool.find_by(id: vid)
+        driver_vanpool_ids.each do |id|
+          if id == vid || !vanpool.driver.nil?
+            added = true
+          end
+        end
+        if !added
+          @current_user.driver_vanpool_ids_will_change!
+          @current_user.driver_vanpool_ids << vid
+          vanpool.driver = @current_user.id
+          vanpool.save
+          @current_user.save
+        end
+      end
+
+      output_msg = output_msg + "AND Volunteer to Drive Selected"
+      redirect_to_welcome = true
+    elsif params[:vanpools_driver].nil?
+      output_msg = output_msg + "AND No vanpool selected to volunteer to drive"
+    end
+
+    flash[:notice] = output_msg
+    if redirect_to_welcome
+      redirect_to pages_welcome_path
+    else
+      redirect_to vanpools_index_path
+    end
   end
 end
